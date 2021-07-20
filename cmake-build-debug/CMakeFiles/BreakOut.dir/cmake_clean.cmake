df3a174f6932e36c753fbab901a2c68222901a19
Rodolfo Gutierrez <rag.ortega@gmail.com>
2021-07-20 12:55:25 -0600

    first commit

---
+++ cmake-build-debug/CMakeFiles/BreakOut.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "BreakOut.exe"
+  "BreakOut.exe.manifest"
+  "BreakOut.pdb"
+  "CMakeFiles/BreakOut.dir/main.cpp.obj"
+  "libBreakOut.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BreakOut.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/BreakOut.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for BreakOut.
+# This may be replaced when dependencies are built.
